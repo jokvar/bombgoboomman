8dd6c2a157af7cf276464050807b7c21fc423623
tavo tevas <jokubas.varnagiris@ktu.lt>
2020-10-07 23:11:00 +0300

    initial commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            message += "a"; 
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMap()
+        {
+            char[] map = "nig".ToCharArray(); 
+            await Clients.All.SendAsync("UpdateMap", map);
+        }
+    }
+}
---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,38 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    //on connect, send random id int and room code
+    //connection.invoke("RegisterPlayer", id, roomCode).catch(function (err) {
+    //    return console.error(err.toString());
+    //});
+    //hub assigns player to room, informs waiting clients about new player
+    //host ui only = when host presses start, game starts
+    //server starts periodically sending map data, gameobject data and player data
+    //clients 
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
