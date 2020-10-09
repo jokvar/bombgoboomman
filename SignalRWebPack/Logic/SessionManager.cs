f962193d013b9c1f7cf702fda9aa1864c22e0e8e
korzal <ceernelius@inbox.lt>
2020-10-09 19:47:40 +0300

    nama jeff

---
+++ SignalRWebPack/Logic/SessionManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalRWebPack.Models;
+
+namespace SignalRWebPack.Logic
+{
+    public class SessionManager //possibly static
+    {
+        public Session GetSession()
+        {
+            Session sess = new Session();
+            return sess;
+        }
+
+        public Session GetSession(string roomCode)
+        {
+            Session sess = new Session();
+            return sess;
+        }
+
+        public string GenerateRoomCode()
+        {
+            return "6969";
+        }
+
+        public void RegisterPlayer(string id)
+        {
+
+        }
+
+        public void RegisterHost(string id)
+        {
+
+        }
+
+    }
+}
