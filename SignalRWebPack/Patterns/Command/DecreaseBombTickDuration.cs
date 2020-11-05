6555d3b67f93d0364c315d1cc0a99bce6360cbd0
Jokūbas Varnagiris <jokubas.varnagiris@ktu.lt>
2020-11-05 23:03:15 +0200

    added concreteCommands, command, invoker

---
+++ SignalRWebPack/Patterns/Command/DecreaseBombTickDuration.cs
@@ -0,0 +1,29 @@
+﻿using SignalRWebPack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRWebPack.Patterns.Command
+{
+    class DecreaseBombTickDuration : PowerupCommand
+    {
+        public DecreaseBombTickDuration(Player _player) : base(_player) { }
+
+        public override void Execute()
+        {
+            if (_player.bombTickDuration > _player.Defaults.MinBombTickDuration)
+            {
+                _player.bombTickDuration--;
+            }
+        }
+
+        public override void Undo()
+        {
+            if (_player.bombTickDuration < _player.Defaults.MaxBombTickDuration)
+            {
+                _player.bombTickDuration++;
+            }
+        }
+    }
+}
