082636fa5b8e11ece2503f07d9c4f8ebdef8476c
Matas Salys <matsal@ktu.lt>
2020-11-06 19:56:07 +0200

    l

--- SignalRWebPack/src/index.ts
+++ SignalRWebPack/src/index.ts
@@ -9,12 +9,14 @@ import "./images/playerTwo.jpg";
 import "./images/playerThree.jpg";
 import "./images/playerFour.jpg";
 import "./images/powerup.jpg";
+import "./images/powerup_bomb_naked.jpg";
+import "./images/powerup_plus.jpg";
 import { ChatHub } from "./chathub/ChatHub";
 import { Renderer } from "./ui/Render";
 import { Input } from "./ui/Input";
 
 
-const version = "v0.1";
+const version = "v0.111";
 
 window.onload = function () {
     var canvas = (document.getElementById('game') as HTMLCanvasElement).getContext("2d");
