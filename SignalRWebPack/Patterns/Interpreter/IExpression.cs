936d0702584d5b1d9187615a3c86e744e0e50b3e
korzal <ceernelius@inbox.lt>
2020-12-05 18:30:52 +0200

    spaghetti interpreter works

---
+++ SignalRWebPack/Patterns/Interpreter/IExpression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalRWebPack.Models;
+
+namespace SignalRWebPack.Patterns.Interpreter
+{
+    interface IExpression
+    {
+        Message InterpretMessage(Message message, string connectionId);
+    }
+}
